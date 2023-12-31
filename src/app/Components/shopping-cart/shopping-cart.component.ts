aa3d1cfa8c5fb9925203c3b596a88a5f50b7070b
Kareem Eltwargy <keltwargy@gmail.com>
2023-12-31 10:50:18 +0200

    Added firebase

--- src/app/Components/shopping-cart/shopping-cart.component.ts
+++ src/app/Components/shopping-cart/shopping-cart.component.ts
@@ -4,7 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CartElementComponent } from '../cart-element/cart-element.component';
 import { Product } from '../../Helpers/products';
 import { ProductService } from '../../Services/product.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { User } from '../../Helpers/users';
 
 @Component({
@@ -23,22 +23,42 @@ import { User } from '../../Helpers/users';
 export class ShoppingCartComponent implements OnInit {
   subTotal = 0;
 
-  User!: User;
+  // test: Product = {
+  //   id: 20,
+  //   name: "Hill's Science Plan Small & Mini Adult Dog Food with Chicken",
+  //   quantity: 11,
+  //   price: 86.25,
+  //   image: '../../../assets/dog-images/20.jpg',
+  //   description:
+  //     "Ingredients: High quality protein and thoughtfully sourced ingredients: Chicken and turkey meal, maize, wheat, brewers' rice, animal fat, digest, vegetable oil, minerals, flaxseed, dried tomato pomace, dried citrus pulp, spinach powder.",
+  //   rating: 4.6,
+  //   reviews: 24,
+  //   categories: 'dog',
+  //   seller: 'Hills',
+  // };
 
+  User!: any;
+  product?: any;
   products: Product[] = [];
 
   constructor(private myProducts: ProductService) {
-    this.myProducts.getAllDogSupplements().subscribe({
-      next: (data) => console.log(data),
-      error: (err) => console.log(err),
-    });
+    myProducts.deleteDogsupplements(20);
+
     this.myProducts.getUser(1).subscribe({
       next: (userData) => {
         this.User = userData;
+        for (const key in this.User) {
+          this.User = this.User[key];
+        }
         for (let i = 0; i < this.User.cart!.length; i++) {
           if (this.User.cart![i].category === 'dog') {
-            this.myProducts.getDogSupplements(this.User.cart![i].id).subscribe({
-              next: (productData) => this.products.push(productData),
+            this.myProducts.getDogSupplements(this.User.cart[i].id).subscribe({
+              next: (productData) => {
+                this.product = productData;
+                for (const key in this.product) {
+                  this.products.push(this.product[key]);
+                }
+              },
               error: () => console.log('Error!'),
             });
           }
--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -1,14 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../Helpers/users';
+import { Product } from '../Helpers/products';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private Dogs_URL =
-    'https://petshop-c2ff5-default-rtdb.firebaseio.com/dogs%20supplements.json';
-  private Users_URL = 'http://localhost:3000/Users';
+    'https://petshop-c2ff5-default-rtdb.firebaseio.com/dogs-supplements.json';
+  private Dogs_URL2 =
+    'https://petshop-c2ff5-default-rtdb.firebaseio.com/dogs-supplements';
+  private Users_URL =
+    'https://petshop-c2ff5-default-rtdb.firebaseio.com/Users.json';
+  private key?: string;
 
   constructor(private httpClient: HttpClient) {}
 
@@ -16,12 +21,44 @@ export class ProductService {
     return this.httpClient.get(this.Dogs_URL);
   }
 
+  addDogSupplements(product: Product) {
+    return this.httpClient.post(this.Dogs_URL, product);
+  }
+
+  updateDogSupplements(product: Product) {
+    return this.httpClient.patch(`${this.Dogs_URL}/${product.id}`, product);
+  }
+
+  // deleteDogSupplements(id: number) {
+  //   return this.httpClient.delete(this.Dogs_URL2 + `/${id}.json`);
+  // }
+  deleteDogsupplements(id: number) {
+    this.getDogSupplements(id).subscribe({
+      next: (productData) => {
+        for (const key in productData) {
+          this.deleteDogSupplement(key).subscribe({
+            next: () => console.log('Deleted'),
+            error: () => console.log('Could not delete'),
+          });
+        }
+      },
+    });
+  }
+
+  private deleteDogSupplement(key: string) {
+    return this.httpClient.delete(this.Dogs_URL2 + `/${key}.json`);
+  }
+
   getDogSupplements(id: number) {
-    return this.httpClient.get(`${this.Dogs_URL}/${id}`);
+    return this.httpClient.get(this.Dogs_URL, {
+      params: new HttpParams().set('orderBy', '"id"').set('equalTo', id),
+    });
   }
 
   getUser(id: number) {
-    return this.httpClient.get(`${this.Users_URL}/${id}`);
+    return this.httpClient.get(this.Users_URL, {
+      params: new HttpParams().set('orderBy', '"id"').set('equalTo', id),
+    });
   }
 
   updateUser(updatedUser: User) {
