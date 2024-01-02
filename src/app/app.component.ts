1cf1ac0153d79925bff5d0ff48595da846e34ffd
Mahmoud Taha <121981608+7Z-7up@users.noreply.github.com>
2024-01-03 01:13:50 +0200

    footer component
    add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, ShoppingCartComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, ShoppingCartComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
