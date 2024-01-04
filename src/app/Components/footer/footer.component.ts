cb9aeaac6cb342902ec93be5a66a053cf4b7885a
a7med3l22 <91208071+a7med3l22@users.noreply.github.com>
2024-01-04 11:15:27 +0200

    translate footer

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -10,17 +10,64 @@ import { TranslationService } from '../../Services/translation.service';
 })
 export class FooterComponent {
   originalText = {
-    paragraph: 'ahmedalaa ',
-    kareem: 'kareem',
+    Aboutus :'Aboutus',
+    HelpSupport  :'Help and Support',
+    Investors:'Investors',
+    Terms:'Terms',
+    PrivacyandPolicy:'Privacy and Policy',
+    CookieSettings:'Cookie Settings',
+    PaymentMethod:'Payment Method',    
+    Home:'Home',   
+    shop:'shop',   
+    About:'About',  
+    Contact:'Contact',
     contactus: 'Contact Us',
+    placeholder:'Type here to search',
+    Getthefreshest :'Get the freshest'   ,
+    News  :'News',
+    Subscribe :'Subscribe',   
+    BusinessPartners: 'Business Partners',
+    NutramaxLaboratories:  'Nutramax Laboratories',
+    NaturVet :'NaturVet',
+    ZestyPaws :'Zesty Paws',
+    ArkNaturals: 'Ark Naturals',
+    WellnessCORE:  'Wellness CORE'        ,
+    HillsScienceDiet:   'Hills Science Diet ' ,
+    Copyright :'Copyright',
+    Allrightsreseved: 'All rights reseved',
+    Paragraph:'we are a pet shop online and we aim to offer our customers a variety of the latest Pet Products online. supplying you with high quality yet budget-friendly products. We offer all of this while providing excellent customer service and friendly support.'
   };
 
   translatedText = {
-    contactus: 'تواصل معنا',
-    kareem: 'كريم',
-    paragraph: 'أحمد علاء',
-  };
+    Aboutus :        ' معلومات عنا ',
+    HelpSupport  :   '   المساعدة والدعم ',
+    Investors:       '   المستثمرين  ' ,
+    Terms:           '    شروط   ',
+    PrivacyandPolicy:' سياسة الخصوصية ',
+    CookieSettings:  '    إعدادات ملفات تعريف الارتباط  ',
+    PaymentMethod:   '      طريقة الدفع او السداد    ',    
+    Home:       'الرئيسية',   
+    shop:       'المتجر ',   
+    About:      ' حول ',  
+    Contact:   ' تواصل معنا ',
+    contactus: '  تواصل معنا ' ,
+    placeholder:'أضغط هنا للبحث',
 
+    Getthefreshest :'ترقية للأحدث'   ,
+    News  :'الأخبار',
+    Subscribe :'إشتراك',   
+    BusinessPartners: 'شركاء العمل',
+    NutramaxLaboratories:  'معامل نيوتراماكس',
+    NaturVet :'الطبيعة البيطرية',
+    ZestyPaws :'زيستي باو',
+    ArkNaturals: 'ارك ناتشورالز',
+    WellnessCORE:  'العافية الأساسية'        ,
+    HillsScienceDiet:   'حمية هيل العلمية ' ,
+    Copyright :'حقوق النشر',
+    Allrightsreseved: 'كل الحقوق محفوظة',
+    Paragraph:' .نحن متجر للحيوانات الأليفة عبر الإنترنت ونهدف إلي أن نقدم لعملائنا مجموعة متنوعة من المنتجات , أحدث منتجات الحيوانات الأليفة علي الإنترنت , تزويدك بجودة عالية حتي الان منتجات صديقية للميزانية , نحن نقدم كل هذا مع تقديم ممتاز خدمة العملاء والدعم الودي '
+  
+  }
   isTranslated = false;
 
   constructor(private translationService: TranslationService) {
